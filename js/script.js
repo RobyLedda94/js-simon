265d67b210f12bb107108b430685823110030803
RobyLedda94 <robyledda01@yahoo.it>
2024-07-09 17:09:28 +0200

    [UPD] ho aggiunto il parseInt per trattare il valore come numero e non come stringa

--- js/script.js
+++ js/script.js
@@ -25,12 +25,16 @@ document.getElementById('numbers').innerText = randomNumbers.join(' - '); // rec
 
 setTimeout (function(){  // definisco la timing function 
     document.getElementById('numbers').innerText = '';  // funzionalità che cancella i numeri dalla pagina
-    let userNumbers = []; // array per raccogliere i numeri inseriti dall'utente 
-    let randomNumbers = [];  // array contenitore numeri random
 
-    for (let i = 0; i < 5; i++) {
-        userNumber = prompt('Inserisci i numeri che hai visto in precedenza'); // prompt per chiedere all'utente di inserire i numeri che ha visto
+    let userNumbers = []; // array per raccogliere i numeri inseriti dall'utente 
+    
+
+    for (let i = 0; i < 5; i++) {  // l'utente inserisce i numeri uno alla volta
+
+        userNumber = parseInt(prompt('Inserisci i numeri che hai visto in precesenza')); // prompt per chiedere all'utente di inserire i numeri che ha visto
+
         userNumbers.push(userNumber); // inserisce i numeri digitati dall'utente nell' array
+
         console.log(userNumbers);
     }
     
