a0c0a27eff8cf7ab8e261f0ae6bf89ea844ed767
RobyLedda94 <robyledda01@yahoo.it>
2024-07-09 14:46:10 +0200

    [UPD] definita la condizione di controllo per evitare l'inserimento di numeri doppi all'interno dell'array

--- js/script.js
+++ js/script.js
@@ -4,8 +4,17 @@
 // Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt(). 
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
+
+
+
+
+
 let randomNumbers = [];  // array contenitore dei numeri 
 
 while (randomNumbers < 5) {  // ciclo while
     let numbers = Math.floor(Math.random() * 100 + 1);  // genero dei numeri da 1 a 100
+
+    if(!randomNumbers.includes(numbers)) {  // condizione di controllo per evitare di inserire numeri doppi all'interno dell'array
+
+    }
 }
--- readme.md
+++ readme.md
@@ -10,3 +10,4 @@ Descrizione: Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 3
 1 - creo un array vuoto che conterrà 5 numeri generati casualmente
 2 - definisco un ciclo while 
 2.1 - genero casualmente dei numeri da 1 a 100 con la libreria math
+2.2 - devo accertarmi che i numeri generati da inserire all'interno dall'array non siano doppi
